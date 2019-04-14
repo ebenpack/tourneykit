2ef2c3ee31af21a84cc353eaed0b92aa4f35f3f0
unknown <ianmnicholas@gmail.com>
2019-04-14 13:41:28 -0400

    Babel config, missing dependencies

--- app/components/app/App.js
+++ app/components/app/App.js
@@ -3,28 +3,24 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
-class AppContainer extends React.Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<Router baseName="/">
-					<App {...this.props} />
-				</Router>
-			</Provider>
-		);
-	}
+const AppContainer = () => {
+	return (
+		<Provider store={store}>
+			<Router baseName="/">
+				<App {...this.props} />
+			</Router>
+		</Provider>
+	);
 };
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="wrap">
-				<div className="app">
+const App = () => {
+	return (
+		<div className="wrap">
+			<div className="app">
 
-				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default AppContainer;
