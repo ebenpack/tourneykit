b38538b71b7fcb14c40fcef496f25cf86a78d1fd
ebenpack <ebenpackwood@gmail.com>
2019-04-21 12:26:38 -0400

    Set CSRF token on spa view

--- tourney/views.py
+++ tourney/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-
+@ensure_csrf_cookie
 def spa(request, path = ""):
 	html = """
 		<!doctype html>
