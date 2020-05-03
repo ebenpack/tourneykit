68cf700ddb6995539c06c0f5e383f330a5a5df53
ebenpack <ebenpackwood@gmail.com>
2020-05-03 12:47:01 -0400

    Add app entry

---
+++ app/src/app.tsx
@@ -0,0 +1,19 @@
+import "@babel/polyfill";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import '../styles/style.scss';
+
+import App from './components/app/App';
+
+declare global {
+    interface Window { startApp: any; }
+}
+
+interface startAppProps {
+    mountPoint: string;
+}
+
+window.startApp = async ({ mountPoint }: startAppProps) => {
+    ReactDOM.render(<App />, document.getElementById(mountPoint));
+};
