74e59d025354abe24c0606c968ef223c421b720e
ebenpack <ebenpackwood@gmail.com>
2020-05-03 12:39:59 -0400

    Black; remove JWT

--- tourneykit/urls.py
+++ tourneykit/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
-from graphql_jwt.decorators import jwt_cookie
 
 from tourney.views import spa
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphql/', jwt_cookie(GraphQLView.as_view(graphiql=True))),
-    path('<path:path>', spa),
-    path('', spa),
+    path("admin/", admin.site.urls),
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
+    path("<path:path>", spa),
+    path("", spa),
 ]
