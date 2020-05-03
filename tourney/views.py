04f5918cbfe57ae4a068cf6e8e6409f1c83cc6d3
ebenpack <ebenpackwood@gmail.com>
2020-05-03 12:40:16 -0400

    Use template

--- tourney/views.py
+++ tourney/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.template.loader import render_to_string
+
 
 @ensure_csrf_cookie
-def spa(request, path = ""):
-    mount_point = 'app'
-    html = f"""
-		<!doctype html>
-		<html>
-		<head>
-			<title>Tourneykit!</title>
-		</head>
-		<body>
-			<div id="{mount_point}"></div>
-			<script src="/static/app.js"></script>
-            <script>window.startApp({{ mountPoint: "{mount_point}" }})</script>
-		</body>
-		</html>
-	"""
-    return HttpResponse(html)
+def spa(request, path=""):
+    rendered = render_to_string("spa_index.html")
+    return HttpResponse(rendered)
