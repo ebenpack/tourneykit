50ea699db0bb0f4b8fe43cb26066d986344ed84f
ebenpack <ebenpackwood@gmail.com>
2020-05-17 14:10:56 -0400

    add self selector

--- app/src/components/auth/authSelectors.ts
+++ app/src/components/auth/authSelectors.ts
@@ -1,3 +1,8 @@
 import { RootState } from "../app/appStore";
 
 export const isLoggedIn = (state: RootState) => state.auth.loggedIn;
+
+export const getSelf = (state: RootState) => ({
+    username: state.auth.username,
+    id: state.auth.id,
+});
